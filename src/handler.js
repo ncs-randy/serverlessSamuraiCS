7e54196be184e00aec7c1ed591175a1aae5f4132
ncs-randy <57424545+ncs-randy@users.noreply.github.com>
2021-05-22 17:47:16 -0700

    added response code

--- src/handler.js
+++ src/handler.js
@@ -3,20 +3,33 @@
 const AWS = require("aws-sdk");
 AWS.config.update({ region: "ap-southeast-1"});
 
-module.exports.TestServerlessLambda = event => {
+module.exports.GetDeliveryEvent = event => {
   console.log(event)
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
+  sendRes( 200, JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully!',
         input: event,
       },
       null,
       2
-    ),
-  };
+    ));
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+const sendRes = (status, body) => {
+  var response = {
+      statusCode: status,
+      headers: {
+          "Content-Type" : "application/json",
+          "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+          "Access-Control-Allow-Methods" : "OPTIONS,POST",
+          "Access-Control-Allow-Credentials" : true,
+          "Access-Control-Allow-Origin" : "*",
+          "X-Requested-With" : "*"
+      },
+      body: body
+  };
+  return response;
 };
